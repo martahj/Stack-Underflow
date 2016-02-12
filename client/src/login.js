eb0a229801c6d90acd7f63b85ffa71e6a76d4766
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-02-12 12:02:28 -0600

    Adding routing and working nav bar

---
+++ client/src/login.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('myApp')
+  .controller('LoginCtrl', ['$scope', function($scope) {
+
+  }]);
---
+++ client/src/logout.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('myApp')
+  .controller('LogoutCtrl', ['$scope', function($scope) {
+
+  }]);
