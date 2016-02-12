719e674acabf055ea7a8c60ee2ff8c103b7ae85b
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-02-12 15:17:57 -0600

    Routing and front-end file structure. CSS reset. Adding broswerify to npm modules.

--- client/app.js
+++ client/app.js
@@ -40,3 +40,5 @@ angular.module('myApp', [
         });
 
 });
+
+require('./src/directives');
--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,22 @@
 
 angular.module('myApp')
   .controller('MainCtrl', ['$scope', function($scope) {
-  	$scope.welcome = 'Welcome to your App!';
-  	$scope.buttonText = 'This is your Button';
-  }]);
+
+    //This is a placeholder for our array of questions. Eventually, we will get these from the server
+  	$scope.questions = [
+      {title: 'A question 1', preview: 'Blahdidity blah blah'},
+      {title: 'A question 2', preview: 'I have  a question'}
+  	];
+
+
+  }])
+  // .directive('sufQuestionPreview', function() {
+
+  // 	return {
+  // 		restrict: 'E',
+  // 		scope: {
+  // 			question: "=info"
+  // 		},
+  // 		templateUrl: './views/directiveViews/questionPreview.html'
+  // 	}
+  // })
