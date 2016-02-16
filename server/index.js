2eed188afd7f5a7661e17d8424a4055c94779c15
Andrew Moon <andrewmoon001@gmail.com>
2016-02-16 12:38:41 -0600

    Authentication working

--- server/index.js
+++ server/index.js
@@ -39,31 +39,13 @@ passport.use(new MakerpassStrategy({
     // console.log('got req', req);
     console.log("GOT TOKEN:", accessToken)
     // console.log('got refreshToken', refreshToken);
-    // console.log('got profile', profile);
+    console.log('got profile', profile._raw);
     // console.log('got done ', done);
     req.session.accessToken  = accessToken;
     req.session.refreshToken = refreshToken;
 
-    //The code below is what the READMEs say, word-for-word. However, User does not appear
-    //to be a module. My guess is that this is something we write ourselves.
-    //Makerpass is getting the user data from github successfully (uncomment the console log for 'got profile' to see it)
-    //I'm guessing we need to maintain our own database of users registered to use Stack Underflow, and this is
-    //what we're supposed to do here - either create a new user or get the info we have stored for that user.
-    //The function done(err, user) will call the function verified on line 171 of passport-makerpass/node_modules/passport-oauth2/strategy.js
-    //In that function:
-      //If there is an error, it will call strategy.error in passport/lib/middleware/authenticate.js
-      //If no user is passed in (ie what is happening now), it will call strategy.fail in that same file
-      //Otherwise, it will call strategy.success
 
-    //   EXAMPLE CODE STARTS HERE
-    // User.findOrCreate({makerpassId: profile.id}, function(err, user) {
-    //   console.log('in findOrCreate cb, err: ', err,' user ', user);
-    //   return done(err, user);
-    // });
-    //   EXAMPLE CODE ENDS HERE
-
-    //This done() statement was in the code before.. my guess is we can take it out after implementing our User module
-    done();
+    done(null, 1);  // Necessary only for serializeUser (see below)
   }
 ));
 
@@ -71,6 +53,12 @@ passport.use(new MakerpassStrategy({
 // Attach Passport to the app
 //
 routes.use(passport.initialize())
+
+//
+// We don't need serializeUser/deserializeUser,
+// but passport will break if we don't write this.
+//
+passport.serializeUser(function(_, done) { done(null, 1) })
 
 //
 // Direct your browser to this route to start the OAuth dance
@@ -84,7 +72,7 @@ routes.get('/auth/makerpass', //what is this?
 //
 //////////////////////////This is calling the failureRedirect, i added /# to the failureRedirect
 routes.get('/auth/makerpass/callback',
-  passport.authenticate('makerpass', { successRedirect: '/#/', failureRedirect: '/#/login', 
+  passport.authenticate('makerpass', { successRedirect: '/#/', failureRedirect: '/#/login',
   failFlash: true }),
   function(req, res) {
     console.log("WORKING")
