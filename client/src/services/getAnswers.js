1e6bbcd07034777c49ac29b26b3f3570a890bc92
Andrew Moon <andrewmoon001@gmail.com>
2016-02-19 12:32:25 -0600

    Reimplemented Marta's last push

--- client/src/services/getAnswers.js
+++ client/src/services/getAnswers.js
@@ -4,8 +4,11 @@ angular.module('myApp')
   .service('GetAnswers', function($http) {
 
     this.getAnswersByQuestion = function(questionId) {
-        //GET request to server for answers to question with questionId
-
+//GET request to server for answers to question with questionId
+     	return $http({
+     		method: 'GET',
+     		url: '/getAnswers/' + questionId
+     	});
         //Return array of answers in format: 
           // {user: __, text: __, timestamp: __}
     };
--- client/src/services/getQuestionDetail.js
+++ client/src/services/getQuestionDetail.js
@@ -5,6 +5,10 @@ angular.module('myApp')
 
     this.getQuestionDetail = function(questionId) {
         //GET request to server for detail on question
+        return $http({
+         	method: 'GET',
+         	url: '/getQuestion/' + questionId
+         })
 
         //Return {publicQuestionId: __, title: __, user: __, text: ___, timestamp: __}
     };
